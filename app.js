2c3eef3cf1789e159d52c398a404fbcd92bc309e
Konee <104918313+KoneeVibes@users.noreply.github.com>
2023-08-29 22:12:12 +0100

    trying to resolve issues around CORS

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ const dbConnect = require('./model/dbConnect');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const corsOptions = require("./config/corsOptions");
+// const corsOptions = require("./config/corsOptions");
 const auth = require('./middleware/auth');
 
 // connect to database
 dbConnect();
 
 // middleware for all routes
-app.use(corsOptions);
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
