9e679045e3ca6abc41b13c632f1f5bc518c8b65e
Konee <104918313+KoneeVibes@users.noreply.github.com>
2023-08-29 23:34:06 +0100

    writing new cors middleware

--- app.js
+++ app.js
@@ -3,24 +3,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const corsOptions = require("./config/corsOptions");
+const cors = require("cors");
+const credentials = require("./middleware/credentials")
 const auth = require('./middleware/auth');
 
 // connect to database
 dbConnect();
 
 // middleware for all routes
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    );
-    next();
-});
+app.use(credentials);
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
