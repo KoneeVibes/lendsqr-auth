d7cbecda345ca69fca3c5ae8bc5c3b0e099f1cc7
Konee <104918313+KoneeVibes@users.noreply.github.com>
2023-08-31 07:32:21 +0100

    refactored cors option to string

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,15 +1,5 @@
 const allowedOrigins = require("./allowedOrigins")
 
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    preflightContinue: true,
-    optionsSuccessStatus: 200
-}
+const corsOptions = "https://lendsqr-ten.vercel.app"
 
 module.exports = corsOptions;
