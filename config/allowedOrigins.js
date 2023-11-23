b791096d49ab6575bf015f14a4cb1ebcee572192
Konee <104918313+KoneeVibes@users.noreply.github.com>
2023-11-23 12:18:30 +0100

    amended https to http in allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     "https://lendsqr-ten.vercel.app",
     "http://127.0.0.1:5500",
-    "https://localhost:3000"
+    "http://localhost:3000"
 ];
 
 module.exports = allowedOrigins;
